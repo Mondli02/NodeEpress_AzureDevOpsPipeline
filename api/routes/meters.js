c2fd28eb9c4dbd39baf4629c9b14965b13a59065
Mondli <gobamndlela@gmail.com>
2020-07-06 06:41:44 +0200

    React and Express api

--- api/routes/meters.js
+++ api/routes/meters.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const mongoose = require('mongoose');
 
-//const Meter =  require('../models/meter');
 
 const metersConroller = require('../controllers/meters');
 
